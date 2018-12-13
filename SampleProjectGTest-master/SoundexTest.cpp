c946e35c42448b8af23d17e0e75cac869d81af28
andrewyoung5 <andrewyoung1992@hotmail.com>
2018-12-13 11:07:56 -0700

    added padding

--- SampleProjectGTest-master/SoundexTest.cpp
+++ SampleProjectGTest-master/SoundexTest.cpp
@@ -8,10 +8,24 @@ using namespace std;
 
 class Soundex
 {
+    /**
+     * @brief pads with zeros
+     * @param word
+     * @return padded word
+     */
+    string zeroPad(const string& word)const
+    {
+       return word +"000";
+    }
     public:
+    /**
+     * @brief encode word
+     * @param word
+     * @return encoded word
+     */
       string encode(const string& word) const
       {
-         return word;
+         return zeroPad(word);
       }
 };
 using ::testing::Eq;
@@ -22,6 +36,15 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
    //act
    auto encode = soundex.encode("A");
    //assert
-   ASSERT_THAT(encode, Eq("A"));
+   ASSERT_THAT(encode, Eq("A000"));
 }
 
+TEST(SoundexEncoding, padsWithZerosToEnsureThreeDigits)
+{
+   //arrange
+   Soundex soundex;
+   //act
+   auto encode = soundex.encode("I");
+   //assert
+   ASSERT_THAT(encode, Eq("I000"));
+}
